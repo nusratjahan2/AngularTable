c7657006d8d337bcb254be09d90e3e2124fe7059
unknown <nusrathaque717@gmail.com>
2025-03-23 03:30:12 +0600

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {MatTableModule} from '@angular/material/table';
+import { DataTableComponent } from './data-table/data-table.component';
+import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [MatTableModule, DataTableComponent, CommonModule, HttpClientModule],
+  standalone: true,
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'Angular_Table';
+ 
+
 }
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,12 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
+import { provideHttpClient } from '@angular/common/http';
+import { importProvidersFrom } from '@angular/core';
+import { MatTableModule } from '@angular/material/table';
 import { AppComponent } from './app/app.component';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+bootstrapApplication(AppComponent, {
+  providers: [
+    provideHttpClient(),
+    importProvidersFrom(MatTableModule)
+  ]
+}).catch(err => console.error(err));
